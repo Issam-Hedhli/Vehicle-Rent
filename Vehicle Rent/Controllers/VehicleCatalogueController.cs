7faa981700e6c274ae7aea90716c21670a54bbd4
issam-hedhli <issam.hedhli@ensi-uma.tn>
2024-04-22 18:50:56 +0100

    add-migration

--- Vehicle Rent/Controllers/VehicleCatalogueController.cs
+++ Vehicle Rent/Controllers/VehicleCatalogueController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using Vehicle_Rent.Models;
-using Vehicle_Rent.Repository.Specific;
 using Vehicle_Rent.Services.EmailSender;
 using Vehicle_Rent.Services.VehicleCatalogue;
 using Vehicle_Rent.ViewModels.VehicleCopyVM;
